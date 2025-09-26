beddacf928d6466e732c7e1cd8e92b2abb1129fd
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2025-09-26 23:18:22 +0530

    Update 3. Find First and Last Position of Element in Sorted Array.java

--- Assignment 1/3. Find First and Last Position of Element in Sorted Array.java
+++ Assignment 1/3. Find First and Last Position of Element in Sorted Array.java
@@ -33,3 +33,19 @@ class Solution {
         return ans;
     }
 }
+
+// Time Complexity : 0 ms;
+// Space Complexity : 42 MB
+
+
+// Example 1:
+// Input: nums = [5,7,7,8,8,10], target = 8
+// Output: [3,4]
+
+// Example 2:
+// Input: nums = [5,7,7,8,8,10], target = 6
+// Output: [-1,-1]
+
+// Example 3:
+// Input: nums = [], target = 0
+// Output: [-1,-1]
