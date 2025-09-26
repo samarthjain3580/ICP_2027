3de60b6b492e3d1c07cea88d8b662d9f5b825256
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2025-09-27 00:01:32 +0530

    Create 452. Minimum Number of Arrows to Burst Balloons.java

---
+++ Assignment 1/452. Minimum Number of Arrows to Burst Balloons.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+        if (points.length == 0) return 0;
+        Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+        
+        int arrows = 1;
+        int end = points[0][1];
+        
+        for (int i = 1; i < points.length; i++) {
+            if (points[i][0] > end) {
+                arrows++;
+                end = points[i][1];
+            }
+        }
+        return arrows;
+    }
+}
+
+// Time Complexity : 54 ms
+// Space Complexity : 68 MB
+
+// Example 1:
+// Input: points = [[10,16],[2,8],[1,6],[7,12]]
+// Output: 2
+// Explanation: The balloons can be burst by 2 arrows:
+// - Shoot an arrow at x = 6, bursting the balloons [2,8] and [1,6].
+// - Shoot an arrow at x = 11, bursting the balloons [10,16] and [7,12].
+
+// Example 2:
+// Input: points = [[1,2],[3,4],[5,6],[7,8]]
+// Output: 4
+// Explanation: One arrow needs to be shot for each balloon for a total of 4 arrows.
+
+// Example 3:
+// Input: points = [[1,2],[2,3],[3,4],[4,5]]
+// Output: 2
+// Explanation: The balloons can be burst by 2 arrows:
+// - Shoot an arrow at x = 2, bursting the balloons [1,2] and [2,3].
+// - Shoot an arrow at x = 4, bursting the balloons [3,4] and [4,5].
