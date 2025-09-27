cf70e4c86d7ac1bdefc71d3388a8e1f9d1d76cba
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2025-09-27 13:06:44 +0530

    Update 2. Sliding Window Maximum.java

--- Assignment 1/2. Sliding Window Maximum.java
+++ Assignment 1/2. Sliding Window Maximum.java
@@ -28,4 +28,23 @@ class Solution {
         }
         return arr;
     }
-}
+} 
+
+// 50 out of 51 test cases passed
+// Example 1:
+
+// Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+// Output: [3,3,5,5,6,7]
+// Explanation: 
+// Window position                Max
+// ---------------               -----
+// [1  3  -1] -3  5  3  6  7       3
+//  1 [3  -1  -3] 5  3  6  7       3
+//  1  3 [-1  -3  5] 3  6  7       5
+//  1  3  -1 [-3  5  3] 6  7       5
+//  1  3  -1  -3 [5  3  6] 7       6
+//  1  3  -1  -3  5 [3  6  7]      7
+// Example 2:
+
+// Input: nums = [1], k = 1
+// Output: [1]
