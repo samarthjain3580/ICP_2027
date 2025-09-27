5522025cb7b40be38fbad6b80ff205f84c965d40
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2025-09-27 21:46:23 +0530

    Update 215. Kth Largest Element in an Array.java

--- Assignment 2/215. Kth Largest Element in an Array.java
+++ Assignment 2/215. Kth Largest Element in an Array.java
@@ -1,3 +1,6 @@
+// Time Complexity : 57 sec
+// Space Complexity : 57 MB
+
 class Solution {
     public int findKthLargest(int[] nums, int k) { 
         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
@@ -12,3 +15,11 @@ class Solution {
         return ans;
     }
 }
+
+// Example 1:
+// Input: nums = [3,2,1,5,6,4], k = 2
+// Output: 5
+
+// Example 2:
+// Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+// Output: 4
