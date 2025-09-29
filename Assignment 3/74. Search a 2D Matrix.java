97862915f06f8b7fced4439e1e55b636408365eb
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2025-09-29 23:34:59 +0530

    Update 74. Search a 2D Matrix.java

--- Assignment 3/74. Search a 2D Matrix.java
+++ Assignment 3/74. Search a 2D Matrix.java
@@ -1,20 +1,18 @@
-// TC : O(n*log(m))
+// TC : O(log(m*n))
 // SC : O(1)
 
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int n = matrix.length;
         int m = matrix[0].length;
-        for(int i=0; i<n; i++) {
-            int left = 0;
-            int right = m-1;
-            while(left <= right) {
-                int mid = (left+right)/2;
-                if (matrix[i][mid] == target) return true;
-                else if (matrix[i][mid] > target) right = mid-1;
-                else left = mid+1; 
-            }
-        }     
+        int left = 0;
+        int right = n*m-1;
+        while(left <= right) {
+            int mid = (left+right)/2;
+            if (matrix[mid/m][mid%m] == target) return true;
+            else if (matrix[mid/m][mid%m] > target) right = mid-1;
+            else left = mid+1; 
+        }   
         return false;
     }
 }
