cab8f49f7e95cd847e522ab18d5c4aba5149b754
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2025-10-02 01:21:20 +0530

    Update 435. Non-overlapping Intervals.java

--- Assignment 5/435. Non-overlapping Intervals.java
+++ Assignment 5/435. Non-overlapping Intervals.java
@@ -1,3 +1,6 @@
+//TC:O(n logn)
+//SC:O(1)
+
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
         Arrays.sort(intervals,(a,b) -> Integer.compare(a[1], b[1]));
@@ -13,3 +16,18 @@ class Solution {
         return c;
     }
 }
+
+// Example 1:
+// Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
+// Output: 1
+// Explanation: [1,3] can be removed and the rest of the intervals are non-overlapping.
+
+// Example 2:
+// Input: intervals = [[1,2],[1,2],[1,2]]
+// Output: 2
+// Explanation: You need to remove two [1,2] to make the rest of the intervals non-overlapping.
+
+// Example 3:
+// Input: intervals = [[1,2],[2,3]]
+// Output: 0
+// Explanation: You don't need to remove any of the intervals since they're already non-overlapping.
