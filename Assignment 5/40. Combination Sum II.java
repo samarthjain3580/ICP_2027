0b9dd91d1f3c31257f0c48f1c76b515be5db5d34
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2025-10-04 00:07:31 +0530

    Update 40. Combination Sum II.java

--- Assignment 5/40. Combination Sum II.java
+++ Assignment 5/40. Combination Sum II.java
@@ -1,3 +1,6 @@
+//TC:O(2^n*n)
+//SC:O(n)
+
 class Solution {
     public List<List<Integer>> combinationSum2(int[] nums, int target) {
         Arrays.sort(nums);
@@ -27,3 +30,11 @@ class Solution {
         }
     }
 }
+
+// Example 1:
+// Input: candidates = [10,1,2,7,6,1,5], target = 8
+// Output: [[1,1,6],[1,2,5],[1,7],[2,6]]
+
+// Example 2:
+// Input: candidates = [2,5,2,1,2], target = 5
+// Output: [[1,2,2],[5]]
