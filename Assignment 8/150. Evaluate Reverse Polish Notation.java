05f223cba098d4ab40f3a485d16430bfd45095d2
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2025-10-08 23:23:35 +0530

    Create 150. Evaluate Reverse Polish Notation.java

---
+++ Assignment 8/150. Evaluate Reverse Polish Notation.java
@@ -0,0 +1,51 @@
+//TC: O(n);
+//SC: O(n);
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        int eval = 0;
+        for(int i=0; i<tokens.length; i++) {
+            if (tokens[i].equals("+")) {
+                stack.push(Integer.valueOf(stack.pop()+stack.pop()));
+            }
+            else if (tokens[i].equals("*")) {
+                stack.push(Integer.valueOf(stack.pop()*stack.pop()));
+            }
+            else if (tokens[i].equals("-")) {
+                int top = stack.pop();
+                stack.push(Integer.valueOf(stack.pop()-top));
+            }
+            else if (tokens[i].equals("/")) {
+                int top = stack.pop();
+                stack.push(Integer.valueOf(stack.pop()/top));
+            }
+            else {
+                stack.push(Integer.valueOf(tokens[i]));
+            }
+        }
+        System.out.println(stack.peek());
+        return stack.peek();
+    }
+}
+
+// Example 1:
+// Input: tokens = ["2","1","+","3","*"]
+// Output: 9
+// Explanation: ((2 + 1) * 3) = 9
+
+// Example 2:
+// Input: tokens = ["4","13","5","/","+"]
+// Output: 6
+// Explanation: (4 + (13 / 5)) = 6
+
+// Example 3:
+// Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+// Output: 22
+// Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+// = ((10 * (6 / (12 * -11))) + 17) + 5
+// = ((10 * (6 / -132)) + 17) + 5
+// = ((10 * 0) + 17) + 5
+// = (0 + 17) + 5
+// = 17 + 5
+// = 22
