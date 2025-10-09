da20337cabb97a68870a3d1cfcdf55dc87a4d257
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2025-10-09 14:26:50 +0530

    Update 71. Simplify Path.java

--- Assignment 8/71. Simplify Path.java
+++ Assignment 8/71. Simplify Path.java
@@ -1,3 +1,6 @@
+// TC: O(n)
+// SC: O(n)
+
 class Solution {
     public String simplifyPath(String path) {
         Stack<String> stack = new Stack<>();
@@ -20,3 +23,28 @@ class Solution {
         return ans.toString();
     }
 }
+
+// Example 1:
+// Input: path = "/home/"
+// Output: "/home"
+// Explanation: The trailing slash should be removed.
+
+// Example 2:
+// Input: path = "/home//foo/"
+// Output: "/home/foo"
+// Explanation: Multiple consecutive slashes are replaced by a single one.
+
+// Example 3:
+// Input: path = "/home/user/Documents/../Pictures"
+// Output: "/home/user/Pictures"
+// Explanation: A double period ".." refers to the directory up a level (the parent directory).
+
+// Example 4:
+// Input: path = "/../"
+// Output: "/"
+// Explanation: Going one level up from the root directory is not possible.
+
+// Example 5:
+// Input: path = "/.../a/../b/c/../d/./"
+// Output: "/.../b/d"
+// Explanation: "..." is a valid name for a directory in this problem.
